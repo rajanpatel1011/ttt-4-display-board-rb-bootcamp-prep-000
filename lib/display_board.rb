f26da77b405e141de8650e38da4b42c7086b9159
Ryan Patel <rajanpatel1011@gmail.com>
2018-04-07 00:05:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(game)
-  puts "#{game[0]} | #{game[1]} | #{game[2]}"
+def display_board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
-  puts "#{game[3]} | #{game[4]} | #{game[5]}"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts "-----------"
-  puts "#{game[6]} | #{game[7]} | #{game[8]}"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
